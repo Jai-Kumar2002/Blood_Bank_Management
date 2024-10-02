99d74da6777eedceadd7249eaefa73996fea934e
JaiKumar <jaikumar414446@gmail.com>
2024-10-02 06:40:59 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,59 @@
+<?php include"config.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include"head.php";?>
+<body>
+
+<?php
+	 include"top_nav.php";
+?>
+	
+    <!-- Page Content -->
+    <div class="container" style="margin-top:70px;">
+  <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header  text-primary">About
+                    <small>Blood Bank</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a>
+                    </li>
+                    <li class="active">About</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="images/contact.jpg" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2 class="text-primary">About Blood Bank</h2>
+                <p>Blood Bank Management System is a Web Based system that can assist the inforamation of blood bag its handling in the blood bank.The term "blood bank" typically refers to a division of a hospital where the storage of blood product occurs and where proper testing is performed (to reduce the risk of transfusion related adverse events). However, it sometimes refers to a collection center, and indeed some hospitals also perform collection.
+</p>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+
+        <hr>
+		<?php include"footer.php"; ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+   
+
+</body>
+
+</html>
